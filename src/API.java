29534b347a7d47020d3b5cec73a39927883158c7
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2023-03-30 15:58:58 -0300

    aula 4

--- src/API.java
+++ src/API.java
@@ -7,7 +7,9 @@ public enum API {
     NASA("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14", //alterar <DEMO_KEY> por sua key (nasa)
             new ExtratorDeConteudoDaNasa()),
     ALURA_IMDB("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json",
-            new ExtratorDeConteudoDoIMDB());
+            new ExtratorDeConteudoDoIMDB()),
+    ALURA_LIMGUAGENS("http://localhost:8080/linguagens",
+            new ExtratorDeConteudoDoALURA());
 
     private String url;
     private IExtratorDeConteudo extrator;
